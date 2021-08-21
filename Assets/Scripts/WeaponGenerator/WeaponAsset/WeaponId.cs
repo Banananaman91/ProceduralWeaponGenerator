c6f9ce5c7819e565004d9fcd59704d15cfb0da75
Ian <barrowian1991@gmail.com>
2021-08-21 11:57:53 +0100

    Added code comments for weapon id

--- Assets/Scripts/WeaponGenerator/WeaponAsset/WeaponId.cs
+++ Assets/Scripts/WeaponGenerator/WeaponAsset/WeaponId.cs
@@ -7,16 +7,20 @@ namespace WeaponGenerator.WeaponAsset
 {
     public class WeaponId : MonoBehaviour
     {
+        //Used purely to allow visibility of part identifications in inspector for debugging
         [SerializeField] private List<string> _weaponIdentification = new List<string>();
 
+        //Weapon Identification List from Weapon Part Identification (PartId)
         public List<string> WeaponIdentification
         {
             get => _weaponIdentification; 
             set => _weaponIdentification = value;
         }
 
+        //Identify specific weapon parts in file explorer, used for more accurate reloading
         public int InstanceId => GetInstanceID();
     
+        //Allows identification of multiple instances of the same weapon
         public int VersionId => Random.Range(0, Int32.MaxValue);
     }
 }
